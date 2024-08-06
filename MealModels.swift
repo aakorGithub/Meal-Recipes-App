d987a4a20bd7793d5655f2db1606ae505ff5dc27
Anthony Akor <115869746+aakorGithub@users.noreply.github.com>
2024-08-06 14:49:20 -0700

    Update MealModels.swift

--- MealModels.swift
+++ MealModels.swift
@@ -15,15 +15,13 @@ struct MealDetail: Codable {
     let strMealThumb: String?
     let strIngredient1: String?
     let strMeasure1: String?
-    // Add additional ingredient and measurement fields up to 20 as needed
-
-    // Combine ingredients and measurements into a readable format
+  
     var ingredients: [String] {
         var results: [String] = []
         if let ingredient1 = strIngredient1, let measure1 = strMeasure1, !ingredient1.isEmpty, !measure1.isEmpty {
             results.append("\(measure1) \(ingredient1)")
         }
-        // Repeat for additional ingredients/measurements
+       
         return results
     }
 }
